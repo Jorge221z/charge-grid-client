98e89f40d6070b073e2df2f4f90ac0e34973b2ec
Jorge221z <jorgemunozcast12@gmail.com>
2026-04-19 20:20:56 +0200

    Add missing methods to StationRepo

--- app/src/main/java/com/jorge/chargegridapp/station/StationRepository.kt
+++ app/src/main/java/com/jorge/chargegridapp/station/StationRepository.kt
@@ -1,17 +1,36 @@
 package com.jorge.chargegridapp.station
 
 import com.jorge.chargegridapp.station.network.StationApi
+import com.jorge.chargegridapp.station.network.dto.StationCreateRequest
+import com.jorge.chargegridapp.station.network.dto.StationDetailResponse
 import com.jorge.chargegridapp.station.network.dto.StationResponse
+import com.jorge.chargegridapp.station.network.dto.StationStatusUpdateRequest
 
 
 // The UI will call these methods to get the data
 class StationRepository(private val stationApi: StationApi) {
 
-    suspend fun fetchAllStations(): List<StationResponse> {
+    suspend fun fetchAllStations(): Result<List<StationResponse>> {
         return runCatching {
             stationApi.getAllStations()
-        }.onFailure { exception ->
-            println("Error fetching stations: ${exception.message}")
-        }.getOrDefault(emptyList()) // Return an empty list if there is an error
+        }
+    }
+
+    suspend fun fetchStationDetail(id: Long): Result<StationDetailResponse> {
+        return runCatching {
+            stationApi.getStationDetail(id)
+        }
+    }
+
+    suspend fun createStation(request: StationCreateRequest): Result<StationResponse> {
+        return runCatching {
+            stationApi.createStation(request)
+        }
+    }
+
+    suspend fun updateStationStatus(id: Long, request: StationStatusUpdateRequest): Result<StationResponse> {
+        return runCatching {
+            stationApi.updateStationStatus(id, request)
+        }
     }
 }
