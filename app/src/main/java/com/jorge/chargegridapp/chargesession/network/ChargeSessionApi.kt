07ebc38383e1a60e7dcba0c07a6b5a2d12ff5dfc
Jorge221z <jorgemunozcast12@gmail.com>
2026-05-02 00:32:49 +0200

    Create new api interface for charge session

--- app/src/main/java/com/jorge/chargegridapp/chargesession/network/ChargeSessionApi.kt
+++ app/src/main/java/com/jorge/chargegridapp/chargesession/network/ChargeSessionApi.kt
@@ -1,4 +1,19 @@
-package com.jorge.chargegridapp.chargesession.network.dto
+package com.jorge.chargegridapp.chargesession.network
+
+import com.jorge.chargegridapp.chargesession.network.dto.ChargeSessionResponse
+import com.jorge.chargegridapp.chargesession.network.dto.StartSessionRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+
+private const val BASE_URL = "api/sessions"
 
 interface ChargeSessionApi {
+
+    @POST("$BASE_URL/start")
+    suspend fun startSession(@Body request: StartSessionRequest): ChargeSessionResponse
+
+    @POST("$BASE_URL/{id}/stop")
+    suspend fun stopSession(@Path("id") id: Long): ChargeSessionResponse
 }
